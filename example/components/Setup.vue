5f57e3043d51816a0b3a3daa99b9ee846cba8bed
rk_zhang <40221744+qmhc@users.noreply.github.com>
2021-06-30 17:42:51 +0800

    test: add script setup example

---
+++ example/components/Setup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="setup" @click="inc">
+    {{ count }}
+  </div>
+  <HelloWorld></HelloWorld>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmit, ref } from 'vue'
+import HelloWorld from './HelloWorld.vue'
+
+const props = defineProps({
+  count: {
+    type: Number,
+    default: 0
+  }
+})
+
+const emit = defineEmit(['on-add'])
+
+const currentCount = ref(props.count)
+
+const inc = () => {
+  currentCount.value++
+  emit('on-add', currentCount.value)
+}
+</script>
--- example/src/index.ts
+++ example/src/index.ts
@@ -7,6 +7,7 @@ export interface Test extends TestBase {
 }
 
 export { default as App } from './App.vue'
+export { default as Setup } from '@/components/Setup.vue'
 export { test } from '@/components/test'
 
 export type { User } from './types'
