010f0425c0705de6d518684b9d728272b1e30060
qmhc <544022268@qq.com>
2021-09-05 13:55:38 +0800

    refactor: create source files through transfrom

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,9 +3,18 @@ import './ignore'
 import DefaultImport from '@components/DefaultImport.vue'
 import type { TestBase } from '@/components/test'
 
+import BothScripts from '@/components/BothScripts.vue'
+import JsTest from '@/components/JsTest.vue'
+import JsxLangTest from '@/components/JsxLangTest.vue'
+import JsxTest from '@/components/JsxTest.jsx'
+import TsxLangTest from '@/components/TsxLangTest.vue'
+import TsxTest from '@/components/TsxTest'
+
 export interface Test extends TestBase {
   count: number
 }
+
+export { addOne, add } from '@/components/js-test.js'
 
 export { default as App } from './App.vue'
 export { default as Setup } from '@/components/Setup.vue'
@@ -14,5 +23,7 @@ export { test } from '@/components/test'
 export type { User } from './types'
 export type { DtsType } from './dts-types'
 export type { Component } from '@/components'
+
+export { BothScripts, JsTest, JsxLangTest, JsxTest, TsxLangTest, TsxTest }
 
 export default DefaultImport
